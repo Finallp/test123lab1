f2327c03882fb06db8c556eb5dd78a1ffa39d8cc
Finallp <an.kr.dm@yandex.ru>
2021-12-03 23:59:56 +0300

    lab01

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,35 @@
-#include <example.hpp>
+#include <student.hpp>
 
-int main() {
-  example();
+#include "gtest/gtest.h"
+#include "iostream"
+
+int main(int argc, char* argv[]) {
+  std::string jsonPath;
+  if (argc<2){
+    throw std::runtime_error{"Empty file path!"};
+  } else {
+    jsonPath = argv[1];
+  }
+
+  std::ifstream file{jsonPath};
+  if (!file) throw std::runtime_error{"Unable to open json: " + jsonPath};
+
+  json data;
+  file >> data;
+  if (!data.at("items").is_array())
+    throw std::runtime_error{"The items in the file must be an array!"};
+  if (data.at("items").size() !=
+      data.at("_meta").at("count").get<size_t>())
+    throw std::runtime_error{"Value in _meta not equal to length of the array"};
+
+  std::vector<student_t> students;
+
+  for (auto const& item : data.at("items")) {
+    auto student = item.get<student_t>();
+    students.push_back(student);
+  }
+  print(students, std::cout);
+
+
+  return 0;
 }
--- tests/example.cpp
+++ tests/example.cpp
@@ -1,11 +1,92 @@
-// Copyright 2021 Your Name <your_email>
-
-#include <stdexcept>
+/// Copyright 2021 Your Name <your_email>
 
 #include <gtest/gtest.h>
 
-#include <example.hpp>
+#include <stdexcept>
+#include <student.hpp>
+const std::string& jsonPath = JFile;
 
-TEST(Example, EmptyTest) {
-    EXPECT_THROW(example(), std::runtime_error);
+TEST(ParserTest, EmptyPath) { EXPECT_FALSE(jsonPath.empty()); }
+TEST(ParserTest, FileExists) {
+  std::ifstream file{jsonPath};
+  EXPECT_FALSE(!file);
+}
+TEST(ParserTest, ItemsInArray) {
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  EXPECT_TRUE(data.at("items").is_array());
+}
+TEST(ParserTest, TrueCount) {
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  EXPECT_EQ(data.at("items").size(),
+            data.at("_meta").at("count").get<size_t>());
+}
+
+TEST(ParserTest, TypeGroup) {
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  std::vector<student_t> students;
+  for (auto const& item : data.at("items")) {
+    auto student = item.get<student_t>();
+    students.push_back(student);
+  }
+  for (auto const& student : students) {
+    bool res = false;
+    if (student.group.type() == typeid(std::string)) {
+      res = true;
+    } else if (student.group.type() == typeid(size_t)) {
+      res = true;
+    }
+    EXPECT_TRUE(res);
+  }
+}
+
+TEST(ParserTest, TypeAvg) {
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  std::vector<student_t> students;
+  for (auto const& item : data.at("items")) {
+    auto student = item.get<student_t>();
+    students.push_back(student);
+  }
+  for (auto const& student : students) {
+    bool res = false;
+    if (student.avg.type() == typeid(std::nullptr_t)) {
+      res = true;
+    } else if (student.avg.type() == typeid(std::string)) {
+      res = true;
+    } else if (student.avg.type() == typeid(double)) {
+      res = true;
+    } else if (student.avg.type() == typeid(size_t)) {
+      res = true;
+    }
+    EXPECT_TRUE(res);
+  }
+}
+
+TEST(ParserTest, TypeDebt) {
+  std::ifstream file{jsonPath};
+  json data;
+  file >> data;
+  std::vector<student_t> students;
+  for (auto const& item : data.at("items")) {
+    auto student = item.get<student_t>();
+    students.push_back(student);
+  }
+  for (auto const& student : students) {
+    bool res = false;
+    if (student.debt.type() == typeid(std::nullptr_t)) {
+      res = true;
+    } else if (student.debt.type() == typeid(std::string)) {
+      res = true;
+    } else if (student.debt.type() == typeid(std::vector<std::string>)) {
+      res = true;
+    }
+    EXPECT_TRUE(res);
+  }
 }
